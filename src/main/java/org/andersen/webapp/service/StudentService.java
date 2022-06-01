311ecf1326887b62d157d5d9aa48857806e37d3e
Alex-Lisniak <lisnyak.am1010a@gmail.com>
2022-06-01 10:29:07 +0300

    add service and view folders+POJO

---
+++ src/main/java/org/andersen/webapp/service/StudentService.java
@@ -0,0 +1,6 @@
+package org.andersen.webapp.service;
+
+
+public class StudentService {
+
+}
---
+++ src/main/java/org/andersen/webapp/view/UserView.java
@@ -0,0 +1,7 @@
+package org.andersen.webapp.view;
+
+public class UserView {
+    public String returnImage(){
+        return null;
+    }
+}
