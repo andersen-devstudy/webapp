64997ec45baed494bb44c3a7231115f2aada6d75
aleksandr-pavliuk <a.pavliuk2@gmail.com>
2022-06-01 13:52:57 +0300

    update create table scheme

--- src/main/resources/sql/init_schema.sql
+++ src/main/resources/sql/init_schema.sql
@@ -2,7 +2,7 @@ drop table if exists groups;
 
 create table users
 (
-    user_id   int,
-    user_name text,
-    user_surname text
+    user_id   SERIAL PRIMARY KEY,
+    user_name TEXT NOT NULL ,
+    user_surname TEXT NOT NULL
 );
