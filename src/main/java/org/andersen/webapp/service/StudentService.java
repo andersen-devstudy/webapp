729764f0f02d5ba7f7aa4536c0ff0ce327ccd15b
aleksandr-pavliuk <a.pavliuk2@gmail.com>
2022-06-02 16:24:31 +0300

    another one awesome commit

--- src/main/java/org/andersen/webapp/service/StudentService.java
+++ src/main/java/org/andersen/webapp/service/StudentService.java
@@ -1,19 +1,13 @@
 package org.andersen.webapp.service;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.java.Log;
-import lombok.extern.slf4j.Slf4j;
 import org.andersen.webapp.dao.datasource.Datasource;
-import org.andersen.webapp.dao.datasource.SimpleDatasource;
 import org.andersen.webapp.dao.impl.UserDaoImpl;
 import org.andersen.webapp.model.User;
 
 import java.sql.SQLException;
 import java.util.List;
 
-import static org.andersen.webapp.utils.TransactionUtils.transaction;
-
-@Slf4j
 @RequiredArgsConstructor
 public class StudentService {
 
@@ -29,7 +23,7 @@ public class StudentService {
     }
   }
 
-  public void delete(Long id){
+  public void delete(Long id) {
     try {
       userDao.deleteById(datasource.getConnection(), id);
     } catch (SQLException e) {
@@ -37,16 +31,17 @@ public class StudentService {
     }
   }
 
-  public User find(Long id){
+  public User find(Long id) {
     try {
-      return userDao.findById(datasource.getConnection(), id).get();
+      return userDao.findById(datasource.getConnection(), id)
+          .get();
     } catch (SQLException e) {
       e.printStackTrace();
       return null;
     }
   }
 
-  public List<User> findAll(){
+  public List<User> findAll() {
     try {
       return userDao.findAll(datasource.getConnection());
     } catch (SQLException e) {
