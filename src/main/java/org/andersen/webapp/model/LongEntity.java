5c8a1657536f786fd2be6c4c9c9e5f64f232cd44
aleksandr-pavliuk <a.pavliuk2@gmail.com>
2022-06-02 14:13:11 +0300

    code formating

--- src/main/java/org/andersen/webapp/model/LongEntity.java
+++ src/main/java/org/andersen/webapp/model/LongEntity.java
@@ -1,12 +1,5 @@
 package org.andersen.webapp.model;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.experimental.SuperBuilder;
-
 abstract public class LongEntity implements Entity<Long> {
 
 }
