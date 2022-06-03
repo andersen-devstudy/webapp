ff86116338392924fc8a230fb3c521de073ca20f
aleksandr-pavliuk <a.pavliuk2@gmail.com>
2022-06-03 11:16:46 +0300

    another one awesome commit

--- src/main/java/org/andersen/webapp/exception/UserServiceException.java
+++ src/main/java/org/andersen/webapp/exception/UserServiceException.java
@@ -2,10 +2,6 @@ package org.andersen.webapp.exception;
 
 import java.sql.SQLException;
 
-/**
- * @author Alex
- * @link https://intvw.me
- */
 public class UserServiceException extends Throwable {
 
   public UserServiceException(String s, SQLException e) {}
--- src/main/java/org/andersen/webapp/servlets/MainServlet.java
+++ src/main/java/org/andersen/webapp/servlets/MainServlet.java
@@ -1,6 +1,11 @@
 package org.andersen.webapp.servlets;
 
 
+import org.andersen.webapp.dao.datasource.SimpleDatasource;
+import org.andersen.webapp.dao.impl.UserDaoImpl;
+import org.andersen.webapp.dao.mappers.UserMapper;
+import org.andersen.webapp.service.StudentService;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,10 +16,12 @@ import java.io.PrintWriter;
 
 @WebServlet(name = "mainServlet", value = "/main")
 public class MainServlet extends HttpServlet {
+    private StudentService service;
 
     private String message;
 
     public void init() {
+        service = new StudentService(new UserDaoImpl(new UserMapper()), new SimpleDatasource());
         message = "Hello World!";
     }
 
