bfd9577b54a609e46d232493b53ea9d0fe854bf7
aleksandr-pavliuk <a.pavliuk2@gmail.com>
2022-06-04 10:46:04 +0300

    rename AddServlet class

--- src/main/java/org/andersen/webapp/servlet/AddServlet.java
+++ src/main/java/org/andersen/webapp/servlet/AddServlet.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet(name = "addingServlet", value = "/adding")
-public class AddingServlet extends HttpServlet {
+public class AddServlet extends HttpServlet {
 
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html");
