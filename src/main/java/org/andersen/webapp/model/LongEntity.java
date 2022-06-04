5bae28782bf1e5c216949c7bee54577250befee0
aleksandr-pavliuk <a.pavliuk2@gmail.com>
2022-06-04 10:51:37 +0300

    refactoring, added lombok annotations in abstractEntity

--- src/main/java/org/andersen/webapp/model/LongEntity.java
+++ src/main/java/org/andersen/webapp/model/LongEntity.java
@@ -1,35 +1,18 @@
 package org.andersen.webapp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Objects;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode
 abstract public class LongEntity implements Entity<Long> {
 
   protected Long id;
 
-  public LongEntity(Long id) {
-    this.id = id;
-  }
-
-  @Override
-  public Long getId() {
-    return id;
-  }
-
-  @Override
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    LongEntity that = (LongEntity) o;
-    return Objects.equals(id, that.id);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id);
-  }
 }
